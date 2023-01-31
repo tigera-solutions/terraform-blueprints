59561732b4526f49914c59b9b612f36b1ee48393
Jonathan Sabo <jonathan@tigera.io>
2023-01-31 11:40:57 -0500

    add aks cluster with overlay networking

---
+++ aks-azure-cni-overlay-networking/main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_version = ">= 1.2.9"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_provider_registration" "overlay" {
+  name = "Microsoft.ContainerService"
+
+  feature {
+    name       = "AzureOverlayPreview"
+    registered = true
+  }
+}
+
+resource "azurerm_resource_group" "vnet" {
+  name     = var.vnet_resource_group_name
+  location = var.location
+}
+
+module "aks_network" {
+  source              = "./modules/vnet"
+  resource_group_name = azurerm_resource_group.vnet.name
+  location            = var.location
+  vnet_name           = var.aks_vnet_name
+  address_space       = ["10.0.0.0/22"]
+  subnets = [
+    {
+      name : "aks-subnet"
+      address_prefixes : ["10.0.0.0/24"]
+    }
+  ]
+}
+
+data "azurerm_kubernetes_service_versions" "current" {
+  location       = var.location
+  version_prefix = var.kube_version_prefix
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                    = var.cluster_name
+  location                = var.location
+  kubernetes_version      = data.azurerm_kubernetes_service_versions.current.latest_version
+  resource_group_name     = azurerm_resource_group.vnet.name
+  dns_prefix              = "aks"
+  private_cluster_enabled = false
+
+  default_node_pool {
+    name           = "default"
+    node_count     = var.nodepool_nodes_count
+    vm_size        = var.nodepool_vm_size
+    vnet_subnet_id = module.aks_network.subnet_ids["aks-subnet"]
+    type           = "VirtualMachineScaleSets"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  linux_profile {
+    admin_username = var.admin_username
+    ssh_key {
+      key_data = var.admin_ssh_key
+    }
+  }
+
+  network_profile {
+    network_plugin      = "azure"
+    network_plugin_mode = "overlay"
+    service_cidr        = var.network_service_cidr
+    dns_service_ip      = var.network_dns_service_ip
+    docker_bridge_cidr  = var.network_docker_bridge_cidr
+  }
+}
+
+resource "azurerm_role_assignment" "netcontributor" {
+  role_definition_name = "Network Contributor"
+  scope                = module.aks_network.subnet_ids["aks-subnet"]
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}

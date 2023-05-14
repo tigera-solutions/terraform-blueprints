e6827584060e1fd52cd17f33a1e47b04664ecd4b
Jonathan Sabo <jonathan@tigera.io>
2023-05-14 10:40:05 -0400

    wip

---
+++ eks-calico-cni-calico-networking/variables.tf
@@ -0,0 +1,29 @@
+variable "name" {
+  description = "Name of cluster - used by Terratest for e2e test automation"
+  type        = string
+  default     = "demo"
+}
+
+variable "region" {
+  description = "AWS Region of cluster"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ssh_keyname" {
+  description = "AWS SSH Keypair Name"
+  type        = string
+  default     = "sabo"
+}
+
+variable "vpc_cidr" {
+  description = "AWS VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "secondary_vpc_cidr" {
+  description = "AWS Secondary VPC CIDR"
+  type        = string
+  default     = "10.99.0.0/16"
+}

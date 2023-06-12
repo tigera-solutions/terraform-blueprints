5afaebca862ef075d6efa1cab3254f2dcbb00b59
Jonathan Sabo <jonathan@tigera.io>
2023-06-12 07:16:29 -0400

    make cidrs more consistent

--- aks-azure-cni-overlay-networking/main.tf
+++ aks-azure-cni-overlay-networking/main.tf
@@ -22,7 +22,7 @@ module "aks_network" {
   resource_group_name = azurerm_resource_group.vnet.name
   location            = var.location
   vnet_name           = var.aks_vnet_name
-  address_space       = ["10.0.0.0/22"]
+  address_space       = ["10.0.0.0/20"]
   subnets = [
     {
       name : "aks-subnet"

93af90aadfbeefa122d5577fb081004127eec582
Jonathan Sabo <jonathan@tigera.io>
2023-06-29 07:17:25 -0400

    add some additional rules to allow outbound dns to the internet and to public fqdns to pull down calico images

--- aks-byo-cni-calico-egress-gateways/azure/modules/firewall/main.tf
+++ aks-byo-cni-calico-egress-gateways/azure/modules/firewall/main.tf
@@ -20,6 +20,7 @@ resource "azurerm_firewall" "fw" {
   }
 }
 
+# This is the only rule required for successful AKS deployment
 resource "azurerm_firewall_application_rule_collection" "fqdntags" {
   name                = "fqdntags"
   azure_firewall_name = azurerm_firewall.fw.name
@@ -33,6 +34,63 @@ resource "azurerm_firewall_application_rule_collection" "fqdntags" {
 
     fqdn_tags = [
       "AzureKubernetesService",
+    ]
+  }
+}
+
+# Additional FQDNS to allow Calico images and other public images to the AKS cluster
+resource "azurerm_firewall_application_rule_collection" "allowed_fqdns" {
+  name                = "allowed-fqdns"
+  azure_firewall_name = azurerm_firewall.fw.name
+  resource_group_name = var.resource_group
+  priority            = 101
+  action              = "Allow"
+
+  rule {
+    name             = "allow access to public fqdns"
+    description      = "allow access to public fqdns"
+    source_addresses = ["*"]
+
+    target_fqdns = [
+      "auth.docker.io",
+      "registry-1.docker.io",
+      "production.cloudflare.docker.com",
+      "quay.io",
+      "*.quay.io",
+      "downloads.tigera.io"
+    ]
+
+    protocol {
+      port = "80"
+      type = "Http"
+    }
+
+    protocol {
+      port = "443"
+      type = "Https"
+    }
+  }
+}
+
+resource "azurerm_firewall_network_rule_collection" "allowed_network_rules" {
+  name                = "allowed-network-rules"
+  azure_firewall_name = azurerm_firewall.fw.name
+  resource_group_name = var.resource_group
+  priority            = 100
+  action              = "Allow"
+
+  rule {
+    description       = "allow access to public networks"
+    name              = "allow access to public networks"
+    source_addresses  = ["*"]
+    destination_ports = ["53"]
+    destination_addresses = [
+      "8.8.8.8",
+      "8.8.4.4",
+    ]
+    protocols         = [
+      "UDP",
+      "TCP",
     ]
   }
 }
